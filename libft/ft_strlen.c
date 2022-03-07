bf174cb24730ff45baeb9cd016a6fcdc3cda0dad
joonhyuk.han <virq@naver.com>
2022-03-08 00:04:07 +0900

    Modify libft ft_strlen.c file
    test: Delete main function to test function result

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,12 +6,9 @@
 /*   By: joonhan <joonhan@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 20:33:18 by joonhan           #+#    #+#             */
-/*   Updated: 2022/03/07 20:36:30 by joonhan          ###   ########.fr       */
+/*   Updated: 2022/03/08 00:03:31 by joonhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <string.h>
-#include <stdio.h>
 
 int	ft_strlen(const	char *s)
 {
@@ -21,14 +18,4 @@ int	ft_strlen(const	char *s)
 	while (s[i] != '\0')
 		i += 1;
 	return (i);
-}
-
-int	main(void)
-{
-	char	str[] = "Hello World";
-
-	printf("str: %s\n", str);
-	printf("strlen: %lu\n", strlen(str));
-	printf("ft_strlen: %d\n", ft_strlen(str));
-	return (0);
 }
