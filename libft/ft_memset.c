16b423488213ba669f3cf71dbe7d781529725b03
Joonhyeok Han <joonhan@c6r7s3.42seoul.kr>
2022-03-18 17:26:02 +0900

    Modify libft ft_memset.c
    test: Add printf to check function return value

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,11 +6,13 @@
 /*   By: joonhan <joonhan@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 13:50:59 by joonhan           #+#    #+#             */
-/*   Updated: 2022/03/14 15:01:34 by joonhan          ###   ########.fr       */
+/*   Updated: 2022/03/18 17:25:21 by joonhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
+#include <stddef.h>
+#include <stdio.h>
 
 void	*ft_memset(void *ptr, int c, size_t len)
 {
@@ -32,8 +34,14 @@ int	main(void)
 	char	str[20];
 	char	str2[20];
 
-	memset(str, 'a', sizeof(str));
-	ft_memset(str2, 'b', sizeof(str2));
+	char	*strPtr;
+	char	*str2Ptr;
+
+	strPtr = memset(str, 'a', sizeof(str));
+	str2Ptr = ft_memset(str2, 'b', sizeof(str2));
+
+	printf("%s\n", strPtr);
+	printf("%s\n", str2Ptr);
 
 	return (0);
 }
