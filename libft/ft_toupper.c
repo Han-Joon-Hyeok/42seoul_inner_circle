048293787c7b543e01c54834979f0bae0b5a8bd3
Joonhyeok Han <joonhan@c6r7s3.42seoul.kr>
2022-03-22 10:52:12 +0900

    Create libft ft_toupper.c

---
+++ libft/ft_toupper.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joonhan <joonhan@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/21 18:31:39 by joonhan           #+#    #+#             */
+/*   Updated: 2022/03/22 10:51:44 by joonhan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <ctype.h>
+
+static int	ft_islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
+
+int	ft_toupper(int c)
+{
+	if (ft_islower(c))
+		return (c - 32);
+	else
+		return (c);
+}
+
+int	main(void)
+{
+	int	arr[11] = {-256, -127, -1, 0, 1, 65, 97, 127, 128, 256, 512};
+	int	size = 11;
+
+	printf("✅ Start\n\n");
+	for (int i = 0; i < size; i++)
+	{
+		printf("char: %c, toupper: %d\n", *(arr + i), toupper(*(arr + i)));
+		printf("char: %c, ft_toupper: %d\n\n", *(arr + i), ft_toupper(*(arr + i)));
+	}
+	return (0);
+}
