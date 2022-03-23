fff6e2703dac05c889d4025ae134978c133e6b3d
Joonhyeok Han <joonhan@c6r6s7.42seoul.kr>
2022-03-23 11:20:14 +0900

    Modify libft ft_strchr.c
    feat: Add ft_strlen function to get src length
    feat: Add ft_strchr function to find a char

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,12 +6,46 @@
 /*   By: joonhan <joonhan@student.42seoul.k>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 17:26:30 by joonhan           #+#    #+#             */
-/*   Updated: 2022/03/22 18:32:28 by joonhan          ###   ########.fr       */
+/*   Updated: 2022/03/23 11:19:00 by joonhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
+#include <stddef.h>
+
+static size_t	ft_strlen(const char *src)
+{
+	size_t	i;
+
+	i = 0;
+	while (*(src + i) != '\0')
+		i += 1;
+	return (i);
+}
+
+char	*ft_strchr(const char *src, int c)
+{
+	char	temp;
+	char	*find;
+	size_t	i;
+	size_t	src_len;
+
+	temp = (char) c;
+	find = NULL;
+	i = 0;
+	src_len = ft_strlen(src);
+	while (i <= src_len)
+	{
+		if (*(src + i) == temp)
+		{
+			find = (char *)(src + i);
+			break ;
+		}
+		i += 1;
+	}
+	return (find);
+}
 
 int	main(void)
 {
@@ -24,6 +58,7 @@ int	main(void)
 	{
 		int c = *(search + i);
 		printf("c: %c, strchr: %p\n", c, strchr(src, c));
+		printf("c: %c, ft_strchr: %p\n\n", c, ft_strchr(src, c));
 	}
 
 	printf("\n\n✅ src2: %s\n\n", src2);
@@ -31,6 +66,7 @@ int	main(void)
 	{
 		int c = *(search + i);
 		printf("c: %c, strchr: %p\n", c, strchr(src2, c));
+		printf("c: %c, ft_strchr: %p\n\n", c, ft_strchr(src2, c));
 	}
 	return (0);
 }
