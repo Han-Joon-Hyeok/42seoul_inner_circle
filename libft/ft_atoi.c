dae0c70273985a155d5857dbf4119db60c21f373
Joonhyeok Han <joonhan@c6r6s7.42seoul.kr>
2022-03-25 10:28:39 +0900

    Modify libft ft_atoi.c
    feat: Add is_numeric function
    feat: Add check_sign function
    feat: Add is_sign_symbol function
    feat: Add is_whitespace function
    feat: Modify ft_atoi function
    test: Add testcase for whitespace string and strings included multiple sign symbols

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: joonhan <joonhan@student.42seoul.k>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 13:06:00 by joonhan           #+#    #+#             */
-/*   Updated: 2022/03/24 14:42:00 by joonhan          ###   ########.fr       */
+/*   Updated: 2022/03/25 10:25:55 by joonhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,64 +14,72 @@
 #include <stddef.h>
 #include <stdio.h>
 
-static int	ft_check_sign(const char *str)
+static int	is_whitespace(char c)
 {
-	if (*(str) == '-')
+	if (c == ' ' || c == '\n' || c == '\r' || c == '\v'
+		|| c == '\f' || c == '\t')
+		return (1);
+	return (0);
+}
+
+static int	is_sign_symbol(char c)
+{
+	if (c == '+' || c == '-')
+		return (1);
+	return (0);
+}
+
+static int	check_sign(char c)
+{
+	if (c == '-')
 		return (-1);
 	return (1);
 }
 
-static int	ft_get_digits(const char *str, int sign)
+static int	is_numeric(char c)
 {
-	int	i;
-
-	i = 0;
-	if (sign == -1)
-		i += 1;
-	while (*(str + i) != '\0')
-	{
-		if (*(str + i) < '0' || *(str + i) > '9')
-			break ;
-		i += 1;
-	}
-	if (sign == -1)
-		return (i - 1);
-	return (i);
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
 }
 
 int	ft_atoi(const char *str)
 {
-	int	sign;
-	int	digits;
 	int	i;
+	int	sign;
+	int	sign_cnt;
 	int	result;
-	int	multiple;
 
-	sign = ft_check_sign(str);
-	digits = ft_get_digits(str, sign);
-	i = digits;
-	if (sign == 1)
-		i = digits - 1;
+	i = 0;
+	sign = 1;
+	sign_cnt = 0;
 	result = 0;
-	multiple = 1;
-	while (i >= 0 && *(str + i) != '-')
+	while (is_whitespace(*(str + i)))
+		i += 1;
+	while (is_sign_symbol(*(str + i)))
 	{
-		result += (*(str + i) - '0') * multiple;
-		multiple *= 10;
-		i -= 1;
+		if (check_sign(*(str + i)) == -1)
+			sign = -1;
+		i += 1;
+		sign_cnt += 1;
 	}
+	if (sign_cnt >= 2)
+		return (0);
+	while (*(str + i) != '\0' && is_numeric(*(str + i)))
+		result = (result * 10) + (*(str + i++) - '0');
 	return (result * sign);
 }
 
 int	main(void)
 {
-	char	src[19][12] = {"-2147483647", "-1000","-12a3b", "-1a2b3", "-100", "-10", "-1", "0", "1", "2", "10", "2147483647", "2147483648", "1a2b3", "12a3b", "123ab", " d_$@#-1123", "abcd", "123-456"};
+	char	src[23][12] = {"-2147483647", "-1000","-12a3b", "-1a2b3", "-100", "-10", "-1", "0", "1", "2", "10", "2147483647", "2147483648", "1a2b3", "12a3b", "123ab", " d_$@#-1123", "\t+ 456", " --+--+456", "\r+-4a56", "\f-45b6", "\v+456", "\r \t \f-456"};
 	size_t	size = sizeof(src) / sizeof(char) / 12;
 
 	for (size_t i = 0; i < size; i++)
 	{
-		printf("src: %s, atoi: %d\n", *(src + i), atoi(*(src + i)));
-		printf("src: %s, ft_atoi: %d\n\n", *(src + i), ft_atoi(*(src + i)));
+		printf("src    : %s\n", *(src + i));
+		printf("atoi   : %d\n", atoi(*(src + i)));
+		printf("ft_atoi: %d\n\n", ft_atoi(*(src + i)));
 	}
 	return (0);
 }
