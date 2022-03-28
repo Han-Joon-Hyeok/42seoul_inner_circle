fb6504466b506dcb59cce769f9cf81a34071a67d
Joonhyeok Han <joonhan@c6r6s7.42seoul.kr>
2022-03-28 17:07:44 +0900

    Modify ft_memmove.c
    chore: Add libft.h
    chore: Change main function into comments

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -3,16 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joonhan <joonhan@student.42seoul.k>        +#+  +:+       +#+        */
+/*   By: joonhan <joonhan@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 17:11:58 by joonhan           #+#    #+#             */
-/*   Updated: 2022/03/22 13:25:10 by joonhan          ###   ########.fr       */
+/*   Updated: 2022/03/28 17:07:28 by joonhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
-#include <stdio.h>
-#include <string.h>
+#include "libft.h"
 
 void	*ft_memmove(void *dest, const void *src, size_t len)
 {
@@ -38,20 +36,20 @@ void	*ft_memmove(void *dest, const void *src, size_t len)
 	return (dest);
 }
 
-int	main(void)
-{
-	char	src[4] = "abc";
-	char	dest[4];
-	char	src2[4] = "abc";
-	char	dest2[4];
-	int		size = 2;
+// int	main(void)
+// {
+// 	char	src[4] = "abc";
+// 	char	dest[4];
+// 	char	src2[4] = "abc";
+// 	char	dest2[4];
+// 	int		size = 2;
 
-	ft_memmove(src + 1, src, sizeof(char) * size);
-	for (int i = 0; i < 3; i++)
-		printf("ft_memmove: %c\n", *(src + i));
+// 	ft_memmove(src + 1, src, sizeof(char) * size);
+// 	for (int i = 0; i < 3; i++)
+// 		printf("ft_memmove: %c\n", *(src + i));
 	
-	memmove(src2 + 1, src2, sizeof(char) * size);
-	for (int j = 0; j < 3; j++)
-		printf("memmove: %c\n", *(src2 + j));
-	return (0);
-}
+// 	memmove(src2 + 1, src2, sizeof(char) * size);
+// 	for (int j = 0; j < 3; j++)
+// 		printf("memmove: %c\n", *(src2 + j));
+// 	return (0);
+// }
