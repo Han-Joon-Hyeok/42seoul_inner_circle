61a54c8f910cbf419c030e45b5f2a7f4b9f9a487
Joonhyeok Han <joonhan@c6r6s7.42seoul.kr>
2022-03-28 17:10:02 +0900

    Modify ft_strlcat.c
    chore: Add libft.h
    chore: Change main function into comments

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -3,26 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joonhan <joonhan@student.42seoul.k>        +#+  +:+       +#+        */
+/*   By: joonhan <joonhan@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 15:32:07 by joonhan           #+#    #+#             */
-/*   Updated: 2022/03/22 16:56:03 by joonhan          ###   ########.fr       */
+/*   Updated: 2022/03/28 17:09:54 by joonhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
-#include <stddef.h>
-
-static size_t	ft_strlen(const char *src)
-{
-	size_t	i;
-
-	i = 0;
-	while (*(src + i) != '\0')
-		i += 1;
-	return (i);
-}
+#include "libft.h"
 
 size_t	ft_strlcat(char *dest, const char *src, size_t destsize)
 {
@@ -49,19 +37,19 @@ size_t	ft_strlcat(char *dest, const char *src, size_t destsize)
 	return (dest_len + src_len);
 }
 
-int	main(void)
-{
-	char	src[4] = "abc";
-	char	dest[4] = "12";
-	char	src2[4] = "abc";
-	char	dest2[4] = "12";
-	int		size[5] = {0, 1, 2, 3, 4};
-	for (int i = 0; i < 5; i++)
-	{
-		int	result = strlcat(dest, src, size[i]);
-		printf("dest: %s, src: %s, strlcat: %d\n", dest, src, result);
-		int	result2 = ft_strlcat(dest2, src2, size[i]);
-		printf("dest2: %s, src2: %s, ft_strlcat: %d\n\n", dest2, src2, result2);
-	}
-	return (0);
-}
+// int	main(void)
+// {
+// 	char	src[4] = "abc";
+// 	char	dest[4] = "12";
+// 	char	src2[4] = "abc";
+// 	char	dest2[4] = "12";
+// 	int		size[5] = {0, 1, 2, 3, 4};
+// 	for (int i = 0; i < 5; i++)
+// 	{
+// 		int	result = strlcat(dest, src, size[i]);
+// 		printf("dest: %s, src: %s, strlcat: %d\n", dest, src, result);
+// 		int	result2 = ft_strlcat(dest2, src2, size[i]);
+// 		printf("dest2: %s, src2: %s, ft_strlcat: %d\n\n", dest2, src2, result2);
+// 	}
+// 	return (0);
+// }
