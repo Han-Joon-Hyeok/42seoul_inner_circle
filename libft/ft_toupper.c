7080b1533dc654d3f073c8b83901e07e26a24ce1
Joonhyeok Han <joonhan@c6r6s7.42seoul.kr>
2022-03-28 17:15:33 +0900

    Modify ft_tolower.c
    chore: Change main function into comments

--- libft/ft_toupper.c
+++ libft/ft_toupper.c
@@ -3,15 +3,12 @@
 /*                                                        :::      ::::::::   */
 /*   ft_toupper.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joonhan <joonhan@student.42seoul.k>        +#+  +:+       +#+        */
+/*   By: joonhan <joonhan@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 18:31:39 by joonhan           #+#    #+#             */
-/*   Updated: 2022/03/22 10:51:44 by joonhan          ###   ########.fr       */
+/*   Updated: 2022/03/28 17:15:25 by joonhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
-#include <ctype.h>
 
 static int	ft_islower(int c)
 {
@@ -29,16 +26,16 @@ int	ft_toupper(int c)
 		return (c);
 }
 
-int	main(void)
-{
-	int	arr[11] = {-256, -127, -1, 0, 1, 65, 97, 127, 128, 256, 512};
-	int	size = 11;
+// int	main(void)
+// {
+// 	int	arr[11] = {-256, -127, -1, 0, 1, 65, 97, 127, 128, 256, 512};
+// 	int	size = 11;
 
-	printf("✅ Start\n\n");
-	for (int i = 0; i < size; i++)
-	{
-		printf("char: %c, toupper: %d\n", *(arr + i), toupper(*(arr + i)));
-		printf("char: %c, ft_toupper: %d\n\n", *(arr + i), ft_toupper(*(arr + i)));
-	}
-	return (0);
-}
+// 	printf("✅ Start\n\n");
+// 	for (int i = 0; i < size; i++)
+// 	{
+// 		printf("char: %c, toupper: %d\n", *(arr + i), toupper(*(arr + i)));
+// 		printf("char: %c, ft_toupper: %d\n\n", *(arr + i), ft_toupper(*(arr + i)));
+// 	}
+// 	return (0);
+// }
