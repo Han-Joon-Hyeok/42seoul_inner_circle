7e6e43235364f59b028ca95a260d3b84a37e7d45
Joonhyeok Han <joonhan@c6r6s7.42seoul.kr>
2022-03-28 17:13:14 +0900

    Modify ft_strrchr.c
    chore: Add libft.h
    chore: Change main function into comments

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -3,26 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joonhan <joonhan@student.42seoul.k>        +#+  +:+       +#+        */
+/*   By: joonhan <joonhan@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 11:39:35 by joonhan           #+#    #+#             */
-/*   Updated: 2022/03/23 12:34:46 by joonhan          ###   ########.fr       */
+/*   Updated: 2022/03/28 17:13:03 by joonhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
-#include <stddef.h>
-
-static size_t	ft_strlen(const char *src)
-{
-	size_t	i;
-
-	i = 0;
-	while (*(src + i) != '\0')
-		i += 1;
-	return (i);
-}
+#include "libft.h"
 
 char	*ft_strrchr(const char *src, int c)
 {
@@ -44,27 +32,27 @@ char	*ft_strrchr(const char *src, int c)
 	return (find);
 }
 
-int	main(void)
-{
-	char	src[5] = "abca";
-	char	src2[5] = "b\0acb";
-	char	search[5] = {'a', 'b', 'c', 'd'};
-	size_t	size = 5;
+// int	main(void)
+// {
+// 	char	src[5] = "abca";
+// 	char	src2[5] = "b\0acb";
+// 	char	search[5] = {'a', 'b', 'c', 'd'};
+// 	size_t	size = 5;
 
-	printf("✅ src: %s\n\n", src);
-	for (int i = 0; i < size; i++)
-	{
-		int c = *(search + i);
-		printf("c: %c, strchr: %p\n", c, strrchr(src, c));
-		printf("c: %c, ft_strrchr: %p\n\n", c, ft_strrchr(src, c));
-	}
+// 	printf("✅ src: %s\n\n", src);
+// 	for (int i = 0; i < size; i++)
+// 	{
+// 		int c = *(search + i);
+// 		printf("c: %c, strchr: %p\n", c, strrchr(src, c));
+// 		printf("c: %c, ft_strrchr: %p\n\n", c, ft_strrchr(src, c));
+// 	}
 	
-	printf("\n\n✅ src2: %s\n\n", src2);
-	for (int i = 0; i < size; i++)
-	{
-		int c = *(search + i);
-		printf("c: %c, strchr: %p\n", c, strrchr(src2, c));
-		printf("c: %c, ft_strrchr: %p\n\n", c, ft_strrchr(src2, c));
-	}	
-	return (0);
-}
+// 	printf("\n\n✅ src2: %s\n\n", src2);
+// 	for (int i = 0; i < size; i++)
+// 	{
+// 		int c = *(search + i);
+// 		printf("c: %c, strchr: %p\n", c, strrchr(src2, c));
+// 		printf("c: %c, ft_strrchr: %p\n\n", c, ft_strrchr(src2, c));
+// 	}	
+// 	return (0);
+// }
