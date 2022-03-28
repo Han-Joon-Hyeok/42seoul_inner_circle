a3ed1b562a65bc1007754a56b62ec25166ba0740
Joonhyeok Han <joonhan@c6r6s7.42seoul.kr>
2022-03-28 16:58:15 +0900

    Modify libft ft_calloc.c
    chore: Add libft.h file
    chore: Change main function into comments
    chore: Remove ft_bzero function

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -3,30 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joonhan <joonhan@student.42seoul.k>        +#+  +:+       +#+        */
+/*   By: joonhan <joonhan@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 10:55:03 by joonhan           #+#    #+#             */
-/*   Updated: 2022/03/25 11:40:45 by joonhan          ###   ########.fr       */
+/*   Updated: 2022/03/28 16:57:17 by joonhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <stddef.h>
-#include <stdio.h>
-
-static void	ft_bzero(void *dest, size_t n)
-{
-	size_t			i;
-	unsigned char	*temp;
-
-	i = 0;
-	temp = (unsigned char *)dest;
-	while (i < n)
-	{
-		*(temp + i) = 0;
-		i += 1;
-	}
-}
+#include "libft.h"
 
 void	*ft_calloc(size_t count, size_t size)
 {
@@ -41,19 +25,19 @@ void	*ft_calloc(size_t count, size_t size)
 	return (ptr);
 }
 
-int	main(void)
-{
-	int	*ptr;
-	int	*ft_ptr;
-	size_t	size = 2;
-	size_t	size_of = sizeof(double);
+// int	main(void)
+// {
+// 	int	*ptr;
+// 	int	*ft_ptr;
+// 	size_t	size = 2;
+// 	size_t	size_of = sizeof(double);
 
-	ptr = calloc(size, size_of);
-	ft_ptr = ft_calloc(size, size_of);
-	for (int i = 0; i < size; i++)
-	{
-		printf("i: %d, ptr   : %d, address: %p\n", i, *(ptr + i), (ptr + i));
-		printf("i: %d, ft_ptr: %d, address: %p\n\n", i, *(ft_ptr + i), (ft_ptr + i));
-	}
-	return (0);
-}
+// 	ptr = calloc(size, size_of);
+// 	ft_ptr = ft_calloc(size, size_of);
+// 	for (int i = 0; i < size; i++)
+// 	{
+// 		printf("i: %d, ptr   : %d, address: %p\n", i, *(ptr + i), (ptr + i));
+// 		printf("i: %d, ft_ptr: %d, address: %p\n\n", i, *(ft_ptr + i), (ft_ptr + i));
+// 	}
+// 	return (0);
+// }
