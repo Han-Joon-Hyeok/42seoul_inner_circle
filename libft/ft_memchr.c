7089f17b507d138d0517ce3b95b4fc566f7b78c0
Joonhyeok Han <joonhan@c6r6s7.42seoul.kr>
2022-03-28 17:03:52 +0900

    Modify ft_memchr.c
    chore: Add libft.h

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -3,16 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joonhan <joonhan@student.42seoul.k>        +#+  +:+       +#+        */
+/*   By: joonhan <joonhan@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 14:17:05 by joonhan           #+#    #+#             */
-/*   Updated: 2022/03/23 14:36:30 by joonhan          ###   ########.fr       */
+/*   Updated: 2022/03/28 17:03:29 by joonhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
-#include <stddef.h>
+#include "libft.h"
 
 void	*ft_memchr(const void *src, int c, size_t n)
 {
@@ -39,17 +37,17 @@ void	*ft_memchr(const void *src, int c, size_t n)
 	return (find);
 }
 
-int	main(void)
-{
-	char	src[6] = "abcde";
-	char	search[6] = "abcdf";
-	int		size = 3;
+// int	main(void)
+// {
+// 	char	src[6] = "abcde";
+// 	char	search[6] = "abcdf";
+// 	int		size = 3;
 
-	printf("✅ src: %s, size: %d\n\n", src, size);
-	for (int i = 0; i < 6; i++)
-	{
-		printf("i: %d, search: %c, memchr: %p\n", i, *(search + i), memchr(src, *(search + i), size));
-		printf("i: %d, search: %c, ft_memchr: %p\n\n", i, *(search + i), ft_memchr(src, *(search + i), size));
-	}
-	return (0);
-}
+// 	printf("✅ src: %s, size: %d\n\n", src, size);
+// 	for (int i = 0; i < 6; i++)
+// 	{
+// 		printf("i: %d, search: %c, memchr: %p\n", i, *(search + i), memchr(src, *(search + i), size));
+// 		printf("i: %d, search: %c, ft_memchr: %p\n\n", i, *(search + i), ft_memchr(src, *(search + i), size));
+// 	}
+// 	return (0);
+// }
