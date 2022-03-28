d188b5cce57b8cf414cc266759003f9808add7a4
Joonhyeok Han <joonhan@c6r6s7.42seoul.kr>
2022-03-28 17:01:11 +0900

    Modify libft ft_isalnum.c

--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -3,44 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joonhan <joonhan@studnet.42seoul.kr>       +#+  +:+       +#+        */
+/*   By: joonhan <joonhan@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 17:50:01 by joonhan           #+#    #+#             */
-/*   Updated: 2022/03/07 23:55:52 by joonhan          ###   ########.fr       */
+/*   Updated: 2022/03/28 17:00:59 by joonhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static int	ft_isdigit(int c)
-{
-	if (c >= '0' && c <= '9')
-		return (1);
-	else
-		return (0);
-}
-
-static int	ft_isupper(int c)
-{
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0);
-}
-
-static int	ft_islower(int c)
-{
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	else
-		return (0);
-}
-
-static int	ft_isalpha(int c)
-{
-	if (ft_isupper(c) || ft_islower(c))
-		return (1);
-	else
-		return (0);
-}
+#include "libft.h"
 
 int	ft_isalnum(int c)
 {
