d85fef4ad19a50a33af11fcddaf1e20529d67c2c
Joonhyeok Han <joonhan@c6r6s7.42seoul.kr>
2022-03-29 15:22:30 +0900

    Create libft ft_lstnew.c

---
+++ libft/ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joonhan <joonhan@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/29 15:11:46 by joonhan           #+#    #+#             */
+/*   Updated: 2022/03/29 15:22:18 by joonhan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*node;
+
+	node = (t_list *)malloc(sizeof(t_list));
+	if (node == NULL)
+		return (NULL);
+	node->next = NULL;
+	node->content = content;
+	return (node);
+}
