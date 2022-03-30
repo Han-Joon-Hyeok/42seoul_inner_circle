7f65806081ff91a33c2d9f0f45f7a8fd223441cf
Joonhyeok Han <joonhan@c6r6s7.42seoul.kr>
2022-03-30 11:26:22 +0900

    Modify ft_lstadd_back.c

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: joonhan <joonhan@student.42seoul.k>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 09:17:33 by joonhan           #+#    #+#             */
-/*   Updated: 2022/03/30 11:02:29 by joonhan          ###   ########.fr       */
+/*   Updated: 2022/03/30 11:26:12 by joonhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,13 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*curr;
 
-	curr = *lst;
-	while (curr->next != NULL)
-		curr = curr->next;
-	curr->next = new;
+	if (*lst == NULL)
+		*lst = new;
+	else
+	{	
+		curr = *lst;
+		while (curr->next != NULL)
+			curr = curr->next;
+		curr->next = new;
+	}
 }
