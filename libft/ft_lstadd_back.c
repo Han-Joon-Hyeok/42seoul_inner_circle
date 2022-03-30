c66f1efcb3783b00c0ce05b0e9139fc2544cb8bb
Joonhyeok Han <joonhan@c6r6s7.42seoul.kr>
2022-03-30 11:03:28 +0900

    Create libft ft_lstadd_back.c

---
+++ libft/ft_lstadd_back.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joonhan <joonhan@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/30 09:17:33 by joonhan           #+#    #+#             */
+/*   Updated: 2022/03/30 11:02:29 by joonhan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*curr;
+
+	curr = *lst;
+	while (curr->next != NULL)
+		curr = curr->next;
+	curr->next = new;
+}
