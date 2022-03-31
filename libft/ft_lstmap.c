a421c8a6313bca969e1324a44b853fabbd3d2670
Joonhyeok Han <joonhan@c6r6s7.42seoul.kr>
2022-03-31 11:34:11 +0900

    Modify libft ft_lstmap.c

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: joonhan <joonhan@student.42seoul.k>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 14:05:36 by joonhan           #+#    #+#             */
-/*   Updated: 2022/03/30 14:59:44 by joonhan          ###   ########.fr       */
+/*   Updated: 2022/03/31 10:59:56 by joonhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,15 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	curr = lst;
 	while (curr != NULL)
 	{
-		node = ft_lstnew(curr->content);
+		node = ft_lstnew(f(curr->content));
 		if (node == NULL)
 		{
 			ft_lstclear(&new_lst, del);
 			return (NULL);
 		}
-		curr = curr->next;
+		ft_lstadd_back(&new_lst, node);
 		node = node->next;
+		curr = curr->next;
 	}
 	return (new_lst);
 }
