3057918dae446b5c02d0f04d3e8a8135df5544bb
Joonhyeok Han <joonhan@c6r6s7.42seoul.kr>
2022-03-31 09:25:24 +0900

    Modify libft libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: joonhan <joonhan@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 16:59:36 by joonhan           #+#    #+#             */
-/*   Updated: 2022/03/30 15:55:15 by joonhan          ###   ########.fr       */
+/*   Updated: 2022/03/31 09:24:32 by joonhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ int			ft_isprint(int c);
 char		*ft_itoa(int n);
 void		*ft_memchr(const void *src, int c, size_t n);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
-void		*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void		*ft_memcpy(void *dst, const void *src, size_t n);
 void		*ft_memmove(void *dest, const void *src, size_t len);
 void		*ft_memset(void *ptr, int c, size_t len);
 void		ft_putchar_fd(char c, int fd);
