d472e80b73600d0e0cc118a834dfe576da774ac9
joonhyuk.han <virq@naver.com>
2022-04-01 14:51:37 +0900

    Modify libft ft_putnbr_fd.c
    feat: Remove ft_itoa function because of using malloc

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,18 +6,55 @@
 /*   By: joonhan <joonhan@student.42seoul.k>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 14:40:39 by joonhan           #+#    #+#             */
-/*   Updated: 2022/03/29 14:49:10 by joonhan          ###   ########.fr       */
+/*   Updated: 2022/04/01 14:49:12 by joonhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	check_sign(int n)
+{
+	if (n >= 0)
+		return (1);
+	else
+		return (-1);
+}
+
+int	get_digits(int n)
+{
+	int	i;
+
+	i = 1;
+	while (n / 10 != 0)
+	{
+		i *= 10;
+		n /= 10;
+	}
+	return (i);
+}
+
 void	ft_putnbr_fd(int n, int fd)
 {
-	size_t	len;
-	char	*str;
+	int	sign;
+	int	digits;
+	int	divisor;
 
-	str = ft_itoa(n);
-	len = ft_strlen(str);
-	write(fd, str, len);
+	sign = check_sign(n);
+	digits = get_digits(n);
+	if (sign == -1)
+		write(fd, "-", 1);
+	while (n > 0 || n < 0)
+	{
+		divisor = ((n / digits) * sign) + '0';
+		write(fd, &divisor, 1);
+		n %= digits;
+		if (n == 0 && digits >= 10)
+			break ;
+		digits /= 10;
+	}
+	while (digits > 0)
+	{
+		write(fd, "0", 1);
+		digits /= 10;
+	}
 }
