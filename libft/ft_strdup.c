bc5f99841dddb31a762060cd216e30e6e18784a6
joonhyuk.han <virq@naver.com>
2022-04-01 14:34:37 +0900

    Modify libft ft_strdup.c
    chore: Delete unused variable

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: joonhan <joonhan@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 11:41:10 by joonhan           #+#    #+#             */
-/*   Updated: 2022/03/30 15:42:35 by joonhan          ###   ########.fr       */
+/*   Updated: 2022/04/01 14:32:42 by joonhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,10 @@ char	*ft_strdup(const char *src)
 {
 	size_t	i;
 	size_t	src_len;
-	char	*temp;
 	char	*ptr;
 
 	i = 0;
 	src_len = ft_strlen(src);
-	temp = (char *)src;
 	ptr = (char *)malloc((src_len * sizeof(char)) + 1);
 	if (ptr == NULL)
 		return (NULL);
