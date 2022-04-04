e1ba84221c8ccb9b1ccf98f836b66c97af0837d0
Joonhyeok Han <joonhan@c6r6s7.42seoul.kr>
2022-04-04 16:27:21 +0900

    Modify libft ft_putstr_fd ft_putendl_fd
    feat: Add checking if string is NULL

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: joonhan <joonhan@student.42seoul.k>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 14:35:08 by joonhan           #+#    #+#             */
-/*   Updated: 2022/03/29 14:38:45 by joonhan          ###   ########.fr       */
+/*   Updated: 2022/04/04 16:26:41 by joonhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	ft_putendl_fd(char *s, int fd)
 {
 	size_t	len;
 
+	if (s == NULL)
+		return ;
 	len = ft_strlen(s);
 	write(fd, s, len);
 	write(fd, "\n", 1);
--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: joonhan <joonhan@student.42seoul.k>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 14:27:51 by joonhan           #+#    #+#             */
-/*   Updated: 2022/03/29 14:33:36 by joonhan          ###   ########.fr       */
+/*   Updated: 2022/04/04 16:21:04 by joonhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	ft_putstr_fd(char *s, int fd)
 {
 	size_t	len;
 
+	if (s == NULL)
+		return ;
 	len = ft_strlen(s);
 	write(fd, s, len);
 }
