09576138c550b6c1977b65e403e978fec20265dc
joonhyuk.han <virq@naver.com>
2022-04-10 10:34:05 +0900

    Refactor ft_strlcpy.c
    feat: Add ft_memcpy function to copy src to dest

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: joonhan <joonhan@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 13:38:51 by joonhan           #+#    #+#             */
-/*   Updated: 2022/03/30 15:15:51 by joonhan          ###   ########.fr       */
+/*   Updated: 2022/04/10 10:08:06 by joonhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,15 @@
 
 size_t	ft_strlcpy(char	*dest, const char	*src, size_t destsize)
 {
-	size_t	i;
 	size_t	src_len;
 
-	i = 0;
 	src_len = ft_strlen(src);
-	while ((i + 1) < destsize && *(src + i) != '\0')
+	if (src_len + 1 < destsize)
+		ft_memcpy(dest, src, src_len + 1);
+	else if (destsize != 0)
 	{
-		*(dest + i) = *(src + i);
-		i += 1;
+		ft_memcpy(dest, src, destsize - 1);
+		*(dest + destsize) = '\0';
 	}
-	if (i < destsize)
-		*(dest + i) = '\0';
 	return (src_len);
 }
