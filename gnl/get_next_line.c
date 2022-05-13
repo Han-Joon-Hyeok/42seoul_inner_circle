f1b8417062ed60fcb21f6e2aba1b53ab29eee73e
joonhyuk.han <virq@naver.com>
2022-05-13 21:59:13 +0900

    Modify get_next_line.c
    feat: Add returning buffer
    test: Add test codes

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -1,6 +1,36 @@
 #include "get_next_line.h"
+#include <fcntl.h>
 
 char	*get_next_line(int fd)
 {
+	int	len;
+	char	*buf;
 
+	buf = (char *)malloc(sizeof(char) * BUFFER_SIZE);
+	if (buf == NULL)
+		return (NULL);
+	len = read(fd, buf, BUFFER_SIZE);
+	if (len <= 0)
+		return (NULL);
+	return (buf);
+}
+
+int	main(void)
+{
+	int	fd;
+	int	result;
+	char	*buf;
+
+	fd = open("hello", O_RDONLY);
+	if (fd == -1)
+		return (-1);
+	result = 1;
+	while (result != 0)
+	{
+		buf = get_next_line(fd);
+		if (buf == NULL)
+			result = 0;
+		write(1, buf, BUFFER_SIZE);
+	}
+	return (0);
 }
