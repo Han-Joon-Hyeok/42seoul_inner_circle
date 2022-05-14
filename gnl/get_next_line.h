85e5d64db22a184747351b36a8bae657c6b0ed08
joonhyuk.han <virq@naver.com>
2022-05-14 20:59:33 +0900

    Update get_next_line.h
    feat: Add t_find struct
    feat: Add t_list struct
    
    feat: Add TRUE FALSE value

--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -1,8 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joonhan <joonhan@studnet.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/14 16:36:55 by joonhan           #+#    #+#             */
+/*   Updated: 2022/05/14 20:57:34 by joonhan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include <stdlib.h>
 # include <unistd.h>
 
+typedef struct s_find
+{
+    int index;
+    int found;
+}   t_find;
+
+typedef struct s_list
+{
+    int             fd;
+    char            *buf;
+    struct s_list   *pLink;
+}   t_list;
+
 char	*get_next_line(int fd);
+
+#endif
+
+#ifndef _COMMON_GNL_DEF_
+# define _COMMON_GNL_DEF_
+
+# define TRUE   1
+# define FALSE  0
 
 #endif
