752462797b323ff4ceede093a72a0911412d6590
joonhyuk.han <virq@naver.com>
2022-05-16 20:04:45 +0900

    Modify get_next_line.h
    refactor: Remove previous content
    refactor: Modify t_node

--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: joonhan <joonhan@studnet.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 16:36:55 by joonhan           #+#    #+#             */
-/*   Updated: 2022/05/15 20:59:19 by joonhan          ###   ########.fr       */
+/*   Updated: 2022/05/16 20:02:58 by joonhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,15 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-typedef struct s_find
-{
-    int index;
-    int found;
-}   t_find;
 typedef struct s_node
 {
     int				fd;
-    char			*backup;
+    char            *backup;
+    char			*buf[BUFFER_SIZE + 1];
 	struct s_node	*next;
 }	t_node;
 
-typedef struct s_list
-{
-    t_node	*header;
-}   t_list;
-
 char	*get_next_line(int fd);
-size_t	ft_strlen(const	char *s);
-char	*ft_strdup(const char *src);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
 
