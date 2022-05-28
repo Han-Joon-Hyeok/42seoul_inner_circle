e33af8183bbc740aba38790de35f66600dab1953
Joonhyeok Han <joonhan@c6r8s8.42seoul.kr>
2022-05-28 19:45:07 +0900

    Modify get_next_line_utils.c
    feat: Add string related functions

--- gnl/get_next_line_utils.c
+++ gnl/get_next_line_utils.c
@@ -11,3 +11,55 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+size_t	ft_strlen(char const *str)
+{
+	size_t	len;
+
+    len = 0;
+	if (str == NULL)
+		return (0);
+	else
+        while (str[len] != '\0')
+            len += 1;
+	return (len);
+}
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t			i;
+	unsigned char	*dest_temp;
+	unsigned char	*src_temp;
+
+	if (n == 0 || dest == src)
+		return (dest);
+	i = 0;
+	dest_temp = (unsigned char *)dest;
+	src_temp = (unsigned char *)src;
+	while (i < n)
+	{
+		*(dest_temp + i) = *(src_temp + i);
+		i += 1;
+	}
+	return (dest);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	char	*ptr;
+
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	ptr = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (ptr == NULL)
+		return (NULL);
+	ft_memcpy(ptr, s1, s1_len);
+	ft_memcpy(ptr + s1_len, s2, s2_len);
+	*(ptr + s1_len + s2_len) = '\0';
+    free(s1);
+	return (ptr);
+}
