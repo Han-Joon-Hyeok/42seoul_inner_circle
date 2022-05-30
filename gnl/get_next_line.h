810bc2d705926e702249fbb914a0d906584597a0
Joonhyeok Han <joonhan@c4r10s8.42seoul.kr>
2022-05-30 19:57:23 +0900

    Update get_next_line.h
    feat: Add char *newline in t_node
    feat: Add ft_strdup

--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -21,6 +21,7 @@ typedef struct s_node
     int				fd;
     char            *backup;
     char			buf[BUFFER_SIZE + 1];
+    char			*newline;
 	struct s_node	*next;
 }	t_node;
 
@@ -28,6 +29,7 @@ char	*get_next_line(int fd);
 size_t	ft_strlen(char const *str);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strdup(const char *src);
 
 #endif
 
