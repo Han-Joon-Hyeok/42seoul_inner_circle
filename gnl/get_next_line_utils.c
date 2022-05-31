8ed93e97ee4ebc7c9a1e74ad2a664118bbd06293
Joonhyeok Han <joonhan@c2r8s7.42seoul.kr>
2022-05-31 21:07:12 +0900

    Modify get_next_line_utils.c
    refactor: Change ft_strjoin first argument data type char * to char **

--- gnl/get_next_line_utils.c
+++ gnl/get_next_line_utils.c
@@ -43,23 +43,27 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	return (dest);
 }
 
-char	*ft_strjoin(char *s1, char *s2)
+char	*ft_strjoin(char **s1, char *s2)
 {
 	size_t	s1_len;
 	size_t	s2_len;
 	char	*ptr;
 
-	if (s1 == NULL && s2 == NULL)
+	if (*s1 == NULL && s2 == NULL)
 		return (NULL);
-	s1_len = ft_strlen(s1);
+	s1_len = ft_strlen(*s1);
 	s2_len = ft_strlen(s2);
 	ptr = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
 	if (ptr == NULL)
 		return (NULL);
-	ft_memcpy(ptr, s1, s1_len);
+	ft_memcpy(ptr, *s1, s1_len);
 	ft_memcpy(ptr + s1_len, s2, s2_len);
 	*(ptr + s1_len + s2_len) = '\0';
-	free(s1);
+	if (*s1 != NULL)
+	{
+		free(*s1);
+		*s1 = NULL;
+	}
 	return (ptr);
 }
 
