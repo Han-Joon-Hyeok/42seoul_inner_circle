0baa5d68b3fdb8898f496d17ba6bfb93d5263ee9
Joonhyeok Han <joonhan@c4r10s5.42seoul.kr>
2022-06-01 19:47:31 +0900

    Modify get_next_line.h
    feat: Add ft_strlcpy function

--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -21,15 +21,15 @@ typedef struct s_node
     int				fd;
     char            *backup;
     char			buf[BUFFER_SIZE + 1];
-    char			*newline;
 	struct s_node	*next;
 }	t_node;
 
 char	*get_next_line(int fd);
 size_t	ft_strlen(char const *str);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
-char	*ft_strjoin(char **s1, char *s2);
+char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strdup(const char *src);
+size_t	ft_strlcpy(char	*dest, const char *src, size_t destsize);
 
 #endif
 
