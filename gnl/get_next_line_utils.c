df74fc1ea1a864a41c151481d61ad0008d0d4ea5
Joonhyeok Han <joonhan@c4r10s5.42seoul.kr>
2022-06-01 19:48:52 +0900

    Modify get_next_line_utils.c
    feat: Change ft_strjoin first argument data type char ** to char *
    feat: Create ft_strlcpy function
    refactor: Add while condition in ft_strdup

--- gnl/get_next_line_utils.c
+++ gnl/get_next_line_utils.c
@@ -43,27 +43,22 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	return (dest);
 }
 
-char	*ft_strjoin(char **s1, char *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
 	size_t	s1_len;
 	size_t	s2_len;
 	char	*ptr;
 
-	if (*s1 == NULL && s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
-	s1_len = ft_strlen(*s1);
+	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
 	ptr = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
 	if (ptr == NULL)
 		return (NULL);
-	ft_memcpy(ptr, *s1, s1_len);
+	ft_memcpy(ptr, s1, s1_len);
 	ft_memcpy(ptr + s1_len, s2, s2_len);
 	*(ptr + s1_len + s2_len) = '\0';
-	if (*s1 != NULL)
-	{
-		free(*s1);
-		*s1 = NULL;
-	}
 	return (ptr);
 }
 
@@ -78,11 +73,26 @@ char	*ft_strdup(const char *src)
 	ptr = (char *)malloc((src_len + 1) * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
-	while (*(src + i) != '\0')
+	while (i < src_len && *(src + i) != '\0')
 	{
 		*(ptr + i) = *(src + i);
 		i += 1;
 	}
 	*(ptr + i) = '\0';
 	return (ptr);
+}
+
+size_t	ft_strlcpy(char	*dest, const char	*src, size_t destsize)
+{
+	size_t	src_len;
+
+	src_len = ft_strlen(src);
+	if (src_len + 1 < destsize)
+		ft_memcpy(dest, src, src_len + 1);
+	else if (destsize != 0)
+	{
+		ft_memcpy(dest, src, destsize - 1);
+		*(dest + destsize - 1) = '\0';
+	}
+	return (src_len);
 }
