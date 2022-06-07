1e8116501880d14d7f188966a989c6ac6ab74940
JoonHyeok Han <joonhan@student.42seoul.kr>
2022-06-07 21:40:31 +0900

    Modify main.c
    test: Add test cases

--- gnl/main.c
+++ gnl/main.c
@@ -7,8 +7,33 @@ int	main(void)
 	int		fd;
     char	*buf;
 
-	printf("41_no_nl: \n");
-	fd = open("./gnlTester/files/41_no_nl", O_RDWR);
+	// printf("\nInvalid fd: \n");
+	// fd = open("./gnlTester/files/empty", O_RDWR);
+    // buf = get_next_line(1000);
+	// printf("1: %s\n", buf);
+	
+    // buf = get_next_line(-1);
+	// printf("2: %s\n", buf);
+	// close(fd);
+
+    // buf = get_next_line(fd);
+	// printf("3: %s\n", buf);
+	// close(fd);
+
+	// printf("\nempty: \n");
+	// fd = open("./gnlTester/files/empty", O_RDWR);
+    // buf = get_next_line(fd);
+	// printf("1: %s\n", buf);
+    // free(buf);
+	
+    // buf = get_next_line(fd);
+	// printf("2: %s\n", buf);
+    // free(buf);
+
+	// close(fd);
+
+	printf("\nnl: \n");
+	fd = open("./gnlTester/files/nl", O_RDWR);
     buf = get_next_line(fd);
 	printf("1: %s\n", buf);
     free(buf);
@@ -16,7 +41,39 @@ int	main(void)
     buf = get_next_line(fd);
 	printf("2: %s\n", buf);
     free(buf);
+
 	close(fd);
+
+	// printf("\n41_no_nl: \n");
+	// fd = open("./gnlTester/files/41_no_nl", O_RDWR);
+    // buf = get_next_line(fd);
+	// printf("1: %s\n", buf);
+    // free(buf);
+	
+    // buf = get_next_line(fd);
+	// printf("2: %s\n", buf);
+    // free(buf);
+
+    // buf = get_next_line(fd);
+	// printf("3: %s\n", buf);
+    // free(buf);
+	// close(fd);
+
+	// printf("\n41_with_nl: \n");
+	// fd = open("./gnlTester/files/41_with_nl", O_RDWR);
+    // buf = get_next_line(fd);
+	// printf("1: %s\n", buf);
+    // free(buf);
+	
+    // buf = get_next_line(fd);
+	// printf("2: %s\n", buf);
+    // free(buf);
+
+    // buf = get_next_line(fd);
+	// printf("3: %s\n", buf);
+    // free(buf);
+	// close(fd);
+
 	// system("leaks a.out");
 	return (0);
 }
