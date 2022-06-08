75bbc5a6ba78c55f67ac66f04b4f4a83276da059
JoonHyeok Han <joonhan@student.42seoul.kr>
2022-06-08 21:16:15 +0900

    Modify get_next_line.c

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -15,81 +15,32 @@
 void	*free_fd(t_node **p_head, int fd)
 {
 	t_node	*prev;
-	t_node	*curr;
 
 	prev = NULL;
-	curr = *p_head;
-	while (curr != NULL)
+	while ((*p_head) != NULL)
 	{
-		if (curr->fd == fd)
+		if ((*p_head)->fd == fd)
 		{
-			if (curr->backup != NULL)
-				free(curr->backup);
-			curr->backup = NULL;
-			curr->next = NULL;
-			free(curr);
-			curr = NULL;
+			if ((*p_head)->backup != NULL)
+				free((*p_head)->backup);
+			free(*p_head);
 			if (prev != NULL)
 				prev->next = NULL;
 			else
 				*p_head = NULL;
 			break ;
 		}
-		prev = curr;
-		curr = curr->next;
-	}
-	return (NULL);
-}
-
-char	*split_newline(t_node *p_node, int i)
-{
-	char	*next;
-	char	*newline;
-	int		backup_len;
-
-	newline = (char *)malloc(sizeof(char) * (i + 2));
-	if (newline == NULL)
-		return (NULL);
-	ft_memcpy(newline, p_node->backup, i + 1);
-	newline[i + 1] = '\0';
-	backup_len = ft_strlen(p_node->backup);
-	if (backup_len - i > 1)
-	{
-		next = (char *)malloc(sizeof(char) * (backup_len - i));
-		if (next == NULL)
-			return (NULL);
-		ft_memcpy(next, &p_node->backup[i + 1], backup_len - i - 1);
-		next[backup_len - i - 1] = '\0';
-		free(p_node->backup);
-		p_node->backup = next;
-	}
-	else
-	{
-		free(p_node->backup);
-		p_node->backup = NULL;
-	}
-	return (newline);
-}
-
-char	*check_newline_in_backup(t_node *p_node, int len)
-{
-	int		i;
-
-	i = 0;
-	while (i < len)
-	{
-		if (p_node->backup[i] == '\n')
-			return (split_newline(p_node, i));
-		i += 1;
+		prev = *p_head;
+		*p_head = (*p_head)->next;
 	}
 	return (NULL);
 }
 
 t_node	*find_fd(t_node **p_head, int fd)
 {
-	t_node	*prev;
+	t_node	*new;
 	t_node	*curr;
-	t_node	*new_node;
+	t_node	*prev;
 
 	prev = NULL;
 	curr = *p_head;
@@ -100,70 +51,129 @@ t_node	*find_fd(t_node **p_head, int fd)
 		prev = curr;
 		curr = curr->next;
 	}
-	new_node = (t_node *)malloc(sizeof(t_node));
-	if (new_node == NULL)
+	new = (t_node *)malloc(sizeof(t_node));
+	if (new == NULL)
 		return (NULL);
-	new_node->fd = fd;
-	new_node->backup = NULL;
-	new_node->next = NULL;
-	if (prev != NULL)
-		prev->next = new_node;
+	new->backup = NULL;
+	new->fd = fd;
+	new->next = NULL;
+	if (*p_head == NULL)
+		*p_head = new;
 	else
-		*p_head = new_node;
-	return (new_node);
+		prev->next = new;
+	return (new);
 }
 
-int	save_in_backup(t_node *p_node, int len)
+char	*split_newline(t_node *p_node, int i)
+{
+	char	*next;
+	char	*newline;
+
+	newline = (char *)malloc(sizeof(char) * (i + 1));
+	if (newline == NULL)
+		return (NULL);
+	newline = ft_memcpy(newline, p_node->backup, i);
+	newline[i] = '\0';
+	if (BUFFER_SIZE - i > 1)
+	{
+		next = (char *)malloc(sizeof(char) * (BUFFER_SIZE - i));
+		if (next == NULL)
+			return (NULL);
+		next = ft_memcpy(next, &p_node->backup[i + 1], BUFFER_SIZE - i - 1);
+		next[BUFFER_SIZE - i - 1] = '\0';
+		if (p_node->backup != NULL)
+			free(p_node->backup);
+		p_node->backup = next;
+	}
+	else
+	{
+		if (p_node->backup != NULL)
+			free(p_node->backup);
+		p_node->backup = NULL;
+	}
+	return (newline);
+}
+
+void	save_in_backup(t_node **p_head, t_node *p_node, ssize_t len)
 {
 	char	*buf;
 	char	*prev_backup;
 
-	if (len < 0)
-		return (-1);
-	buf = (char *)malloc(sizeof(char) * (len + 1));
+	if (len == 0)
+		return ;
+	buf = (char *)malloc(sizeof(len + 1));
 	if (buf == NULL)
-		return (-1);
-	ft_memcpy(buf, p_node->buf, len);
+		free_fd(p_head, p_node->fd);
+	buf = ft_memcpy(buf, p_node->buf, len);
 	buf[len] = '\0';
-	if (p_node->backup != NULL)
+	if (p_node->backup == NULL)
+		p_node->backup = buf;
+	else
 	{
 		prev_backup = ft_strdup(p_node->backup);
 		free(p_node->backup);
 		p_node->backup = ft_strjoin(prev_backup, buf);
-		free(prev_backup);
+		free(buf);
 	}
-	else
-		p_node->backup = ft_strdup(buf);
-	free(buf);
-	return (len);
+}
+
+int	check_newline_in_backup(t_node **p_head, t_node *p_node, ssize_t len)
+{
+	int	i;
+	int	found;
+
+	save_in_backup(p_head, p_node, len);
+	i = 0;
+	found = FALSE;
+	while (p_node->backup != NULL && p_node->backup[i] != '\0')
+	{
+		if (p_node->backup[i] == '\n')
+		{
+			found = TRUE;
+			break ;
+		}
+		i += 1;
+	}
+	if (found == FALSE)
+		return (FALSE);
+	return (i);
+}
+
+char	*clear_backup(t_node **p_head, int fd)
+{
+	t_node	*p_node;
+	char	*newline;
+
+	p_node = find_fd(p_head, fd);
+	if (p_node->backup == NULL)
+		return (free_fd(p_head, fd));
+	newline = ft_strdup(p_node->backup);
+	free_fd(p_head, fd);
+	return (newline);
 }
 
 char	*get_next_line(int fd)
 {
-	int				len;
+	ssize_t			len;
 	t_node			*p_node;
 	static t_node	*p_head;
-	char			*newline;
+	int				found_newline;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	p_node = find_fd(&p_head, fd);
 	if (p_head == NULL || p_node == NULL)
 		return (free_fd(&p_head, fd));
-	len = save_in_backup(p_node, read(fd, p_node->buf, BUFFER_SIZE));
-	if (len <= 0)
+	len = read(fd, p_node->buf, BUFFER_SIZE);
+	if (len < 0)
 		return (free_fd(&p_head, fd));
 	while (len >= 0)
 	{
-		if (len == 0)
-		{
-			newline = ft_strdup(p_node->backup);
-			free_fd(&p_head, fd);
-			return (newline);
-		}
-		newline = check_newline_in_backup(p_node, len);
-		if (newline != NULL)
-			return (newline);
+		found_newline = check_newline_in_backup(&p_head, p_node, len);
+		if (found_newline == TRUE)
+			return (split_newline(p_node, found_newline));
+		if (len != BUFFER_SIZE)
+			return (clear_backup(&p_head, fd));
 		len = read(fd, p_node->buf, BUFFER_SIZE);
 	}
 	return (NULL);
--- gnl/main.c
+++ gnl/main.c
@@ -5,75 +5,65 @@
 int	main(void)
 {
 	int		fd;
-    char	*buf;
+	char	*buf;
 
 	// printf("\nInvalid fd: \n");
 	// fd = open("./gnlTester/files/empty", O_RDWR);
-    // buf = get_next_line(1000);
+	// buf = get_next_line(1000);
 	// printf("1: %s\n", buf);
 	
-    // buf = get_next_line(-1);
+	// buf = get_next_line(-1);
 	// printf("2: %s\n", buf);
 	// close(fd);
 
-    // buf = get_next_line(fd);
+	// buf = get_next_line(fd);
 	// printf("3: %s\n", buf);
-	// close(fd);
 
 	// printf("\nempty: \n");
 	// fd = open("./gnlTester/files/empty", O_RDWR);
-    // buf = get_next_line(fd);
+	// buf = get_next_line(fd);
 	// printf("1: %s\n", buf);
-    // free(buf);
+	// free(buf);
 	
-    // buf = get_next_line(fd);
+	// buf = get_next_line(fd);
 	// printf("2: %s\n", buf);
-    // free(buf);
+	// free(buf);
+	// close(fd);
+
+	// printf("\nnl: \n");
+	// fd = open("./gnlTester/files/nl", O_RDWR);
+	// buf = get_next_line(fd);
+	// printf("1: %s\n", buf);
+	// free(buf);
+	
+	// buf = get_next_line(fd);
+	// printf("2: %s\n", buf);
+	// free(buf);
 
 	// close(fd);
 
-	printf("\nnl: \n");
-	fd = open("./gnlTester/files/nl", O_RDWR);
-    buf = get_next_line(fd);
+	printf("\n41_no_nl: \n");
+	fd = open("./gnlTester/files/41_no_nl", O_RDWR);
+	buf = get_next_line(fd);
 	printf("1: %s\n", buf);
-    free(buf);
+	free(buf);
 	
-    buf = get_next_line(fd);
+	buf = get_next_line(fd);
 	printf("2: %s\n", buf);
-    free(buf);
-
+	free(buf);
 	close(fd);
-
-	// printf("\n41_no_nl: \n");
-	// fd = open("./gnlTester/files/41_no_nl", O_RDWR);
-    // buf = get_next_line(fd);
-	// printf("1: %s\n", buf);
-    // free(buf);
-	
-    // buf = get_next_line(fd);
-	// printf("2: %s\n", buf);
-    // free(buf);
-
-    // buf = get_next_line(fd);
-	// printf("3: %s\n", buf);
-    // free(buf);
-	// close(fd);
 
 	// printf("\n41_with_nl: \n");
 	// fd = open("./gnlTester/files/41_with_nl", O_RDWR);
-    // buf = get_next_line(fd);
+	// buf = get_next_line(fd);
 	// printf("1: %s\n", buf);
-    // free(buf);
+	// free(buf);
 	
-    // buf = get_next_line(fd);
+	// buf = get_next_line(fd);
 	// printf("2: %s\n", buf);
-    // free(buf);
-
-    // buf = get_next_line(fd);
-	// printf("3: %s\n", buf);
-    // free(buf);
+	// free(buf);
 	// close(fd);
 
-	// system("leaks a.out");
+	system("leaks a.out");
 	return (0);
 }
