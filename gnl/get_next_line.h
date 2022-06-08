d2308c846be40a35a0a601fa64563d6f581d5ff3
joonhyuk.han <virq@naver.com>
2022-06-09 01:16:40 +0900

    Modify get_next_line
    feat: ft_strdup 함수 삭제
    feat: ft_memcpy 맨 끝에 널값 추가 기능(테스터기 KO 확인 필요)
    feat: utils 파일에서 include 하는 헤더 파일 변경

--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: joonhan <joonhan@studnet.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 16:36:55 by joonhan           #+#    #+#             */
-/*   Updated: 2022/06/08 23:39:51 by joonhan          ###   ########.fr       */
+/*   Updated: 2022/06/09 01:11:25 by joonhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,6 @@ char	*get_next_line(int fd);
 size_t	ft_strlen(char const *str);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 char	*ft_strjoin(char *s1, char *s2);
-char	*ft_strdup(const char *src);
 
 #endif
 
