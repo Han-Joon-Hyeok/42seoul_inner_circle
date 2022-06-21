1ae7f9a448b6deb48b7304675165d4944c563ad2
joonhyuk.han <virq@naver.com>
2022-06-21 11:57:51 +0900

    Modify ft_printf.c
    feat: %i, %u, %x, %X, %% 구현
    refactor: norminette 맞추기 위해 ft_printf_args 함수 추가

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,6 +1,4 @@
-#include "./libft/libft.h"
-#include <stdio.h>
-#include <stdarg.h>
+#include "ft_printf.h"
 
 void	ft_printf_va_c(size_t *idx, char c)
 {
@@ -29,10 +27,99 @@ void	ft_printf_va_p(size_t *idx, void *p)
 {
 	size_t	addr;
 
-	addr = *(unsigned long *)&p;
+	addr = *(size_t *)&p; // size_t 자료형이 테스터기에서 작동하는지 확인 필요
 	ft_putstr_fd("0x", 1);
 	print_recursive_hex(addr);
 	*idx += 2;
+}
+
+void	ft_printf_va_d(size_t *idx, int n)
+{
+	ft_putnbr_fd(n, 1);
+	*idx += 2;
+}
+
+void	ft_printf_va_i(size_t *idx, int n)
+{
+	ft_printf_va_d(idx, n);
+}
+
+void	ft_printf_recursive_va_u(size_t *idx, unsigned int n)
+{
+	char	c;
+
+	if (n == 0)
+	{
+		*idx += 2;
+		return ;
+	}
+	ft_printf_recursive_va_u(idx, n / 10);
+	c = (n % 10) + '0';
+	write(1, &c, 1);
+}
+
+void	ft_printf_va_u(size_t *idx, unsigned int n)
+{
+	if (n == 0)
+	{
+		write(1, "0", 1);
+		*idx += 2;
+		return ;
+	}
+	ft_printf_recursive_va_u(idx, n);
+}
+
+void	ft_printf_recursive_va_x(size_t *idx, unsigned int n, char c)
+{
+	char	*hex;
+
+	if (c == 'x')
+		hex = "0123456789abcdef";
+	else
+		hex = "0123456789ABCDEF";
+	if (n == 0)
+	{
+		*idx += 2;
+		return ;
+	}
+	ft_printf_recursive_va_x(idx, n / 16, c);
+	write(1, &hex[n % 16], 1);
+}
+
+void	ft_printf_va_x(size_t *idx, unsigned int n, char c)
+{
+	if (n == 0)
+	{
+		write(1, "0", 1);
+		*idx += 2;
+		return ;
+	}
+	ft_printf_recursive_va_x(idx, n, c);
+}
+
+void	ft_printf_args(char format, size_t *idx, va_list ap)
+{
+	if (format == 'c')
+		ft_printf_va_c(idx, (char)va_arg(ap, int));
+	else if (format == 's')
+		ft_printf_va_s(idx, (char *)va_arg(ap, int *));
+	else if (format == 'p')
+		ft_printf_va_p(idx, (void *)va_arg(ap, void *));
+	else if (format == 'd')
+		ft_printf_va_d(idx, (int)va_arg(ap, int));
+	else if (format == 'i')
+		ft_printf_va_i(idx, (int)va_arg(ap, int));
+	else if (format == 'u')
+		ft_printf_va_u(idx, (unsigned int)va_arg(ap, unsigned int));
+	else if (format == 'x')
+		ft_printf_va_x(idx, (unsigned int)va_arg(ap, unsigned int), 'x');
+	else if (format == 'X')
+		ft_printf_va_x(idx, (unsigned int)va_arg(ap, unsigned int), 'X');
+	else if (format == '%')
+	{
+		write(1, "%", 1);
+		*idx += 2;
+	}
 }
 
 int	ft_printf(const char *str, ...)
@@ -40,23 +127,18 @@ int	ft_printf(const char *str, ...)
 	size_t	idx;
 	size_t	str_len;
 	va_list	ap;
+	va_list	ap2;
 
 	idx = 0;
 	str_len = ft_strlen(str);
 	va_start(ap, str);
 	while (idx < str_len && str[idx] != '\0')
 	{
-		if (ft_strnstr(&str[idx], "%c", 2))
+		if ((str[idx] == '%') && ft_strchr("cspdiuxX%", str[idx + 1]))
 		{
-			ft_printf_va_c(&idx, (char)va_arg(ap, int));
-		}
-		else if (ft_strnstr(&str[idx], "%s", 2))
-		{
-			ft_printf_va_s(&idx, (char *)va_arg(ap, int *));
-		}
-		else if (ft_strnstr(&str[idx], "%p", 2))
-		{
-			ft_printf_va_p(&idx, (void *)va_arg(ap, void *));
+			va_copy(ap2, ap);
+			ft_printf_args(str[idx + 1], &idx, ap2);
+			va_end(ap2);
 		}
 		else
 		{
@@ -80,14 +162,35 @@ int	main(void)
 	*/
 	// printf("%d %c", 1);			// 이 경우도 마찬가지로 
 	// printf("hello %Q", 1234);	// 출력이 되지 않아야 한다.
-	// printf("%%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %c%%", 'A', "42", 42, 42 ,42 , 42, 42, 'B', "-42", -42, -42 ,-42 ,-42, 42, 'C', "0", 0, 0 ,0 ,0, 42, 0);
-	printf("printf c	: hello %c\n", 'a');
-	ft_printf("ft_printf c	: hello %c\n", 'a');
+	// printf("%%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %c%%\n", 'A', "42", 42, 42 ,42 , 42, 42, 'B', "-42", -42, -42 ,-42 ,-42, 42, 'C', "0", 0, 0 ,0 ,0, 42, 0);
+	// printf("printf c	: hello %c\n", 'a');
+	ft_printf("ft_printf c	: hello %c\n\n", 'a');
 
-	printf("printf s	: %s\n", "hello 42");
-	ft_printf("ft_printf s	: %s\n", "hello 42");
+	// printf("printf s	: %s\n", "hello 42");
+	ft_printf("ft_printf s	: %s\n\n", "hello 42");
 
-	printf("printf p	: %p\n", &num);
-	ft_printf("ft_printf p	: %p\n", &num);
+	// printf("printf p	: %p\n", &num);
+	ft_printf("ft_printf p	: %p\n\n", &num);
+
+	// printf("printf d	: %d\n", num);
+	ft_printf("ft_printf d	: %d\n\n", num);
+
+	// printf("printf d	: %d\n", num);
+	ft_printf("ft_printf d	: %d\n\n", num);
+
+	// printf("printf i	: %i\n", num);
+	ft_printf("ft_printf i	: %i\n\n", num);
+
+	// printf("printf u	: %u\n", 1000);
+	ft_printf("ft_printf u	: %u\n\n", 1000);
+
+	// printf("printf x	: %x\n", 2147483647);
+	ft_printf("ft_printf x	: %x\n\n", 2147483647);
+
+	// printf("printf X	: %X\n", 2147483647);
+	ft_printf("ft_printf X	: %X\n\n", 2147483647);
+
+	// printf("printf percent		: %%\n");
+	ft_printf("ft_printf percent	: %%\n\n");
 	return (0);
 }
