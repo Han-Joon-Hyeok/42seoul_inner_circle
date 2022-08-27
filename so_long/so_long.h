728d3a631e3cef0029a2a70f6a5a5e2f8f2721bf
joonhyuk.han <virq@naver.com>
2022-08-27 15:08:31 +0900

    Modify so_long.h
    feat: Add functions
    feat: Remove mlx struct
    feat: Add variables to game struct

--- so_long/so_long.h
+++ so_long/so_long.h
@@ -6,7 +6,7 @@
 /*   By: joonhan <joonhan@studnet.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 16:46:52 by joonhan           #+#    #+#             */
-/*   Updated: 2022/08/25 17:49:41 by joonhan          ###   ########.fr       */
+/*   Updated: 2022/08/27 15:00:09 by joonhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,13 @@
 
 # define X_EVENT_KEY_PRESS		2
 # define X_EVENT_KEY_RELEASE	3
+# define X_EVENT_KEY_EXIT		17
 
-# define WINDOW_WIDTH		128
-# define WINDOW_HEIGHT		128
+# define WINDOW_WIDTH		500
+# define WINDOW_HEIGHT		500
+
+# define IMAGE_WIDTH		16
+# define IMAGE_HEIGHT		16
 
 typedef struct s_pos
 {
@@ -37,30 +41,38 @@ typedef struct s_pos
 	size_t	moves;
 }	t_pos;
 
-typedef struct s_mlx
-{
-	void	*mlx_ptr;
-	void	*win_ptr;
-}	t_mlx;
-
 typedef struct s_game
 {
-	void	*land;
-	void	*wall;
-	void	*character;
-	void	*collectible;
-	void	*door;
-	void	*fire;
-	char	*map;
-	size_t	width;
-	size_t	height;
-	size_t	map_size;
+	t_pos		*pos;
+	ssize_t		offset[14];
+	void		*mlx;
+	void		*win;
+	void		*land;
+	void		*wall;
+	void		*chr;
+	void		*collectible;
+	void		*door;
+	void		*fire;
+	char		*map;
+	size_t		map_width;
+	size_t		map_height;
+	size_t		map_size;
+	size_t		window_width;
+	size_t		window_height;
+	size_t		player_idx;
 }	t_game;
 
-void	print_error(char *msg);
-void	is_valid_map(char *map_file, t_game *game);
-void	init_pos(t_pos *pos);
-void	init_mlx(t_mlx *mlx);
-void	init_images(t_mlx *mlx, t_game *game);
+void	print_msg(char *msg);
+void	print_error(char *msg, void *game);
+void	ft_strjoin_without_newline(t_game *game, char *line, size_t width);
+void	save_map(char *map_file, t_game *game);
+void	is_valid_map(t_game *game);
+void	init_window_size(t_game *game);
+void	init_pos(t_game *game);
+void	init_mlx(t_game *game);
+void	init_images(t_game *game);
+void	draw_map(t_game *game);
+int		main_loop_hook(int key_code, t_game *game);
+int		exit_hook(t_game *game);
 
 #endif
