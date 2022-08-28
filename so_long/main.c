114b388d612d64aff23af74917d877428e7e1aa8
joonhyuk.han <virq@naver.com>
2022-08-28 21:44:44 +0900

    Modify main.c
    feat: Create init_setting function

--- so_long/main.c
+++ so_long/main.c
@@ -6,43 +6,19 @@
 /*   By: joonhan <joonhan@studnet.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 16:33:31 by joonhan           #+#    #+#             */
-/*   Updated: 2022/08/28 14:38:21 by joonhan          ###   ########.fr       */
+/*   Updated: 2022/08/28 21:13:52 by joonhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	get_total_collect_count(t_game *game)
+static void	init_setting(t_game *game)
 {
-	size_t	count;
-	size_t	idx;
-
-	count = 0;
-	idx = 0;
-	while (idx < game->map_size)
-	{
-		if (game->map[idx] == 'C')
-			count++;
-		idx++;
-	}
-	game->total_collect_count = count;
-	game->curr_collect_count = 0;
-}
-
-void	get_player_idx(t_game *game)
-{
-	char	*current_player_idx;
-
-	current_player_idx = ft_strchr(game->map, 'P');
-	game->player_idx = current_player_idx - game->map;
-}
-
-void	init_offset(t_game *game)
-{
-	game->offset[KEY_W] = -((ssize_t)game->map_width);
-	game->offset[KEY_A] = -1;
-	game->offset[KEY_S] = ((ssize_t)game->map_width);
-	game->offset[KEY_D] = 1;
+	init_window_size(game);
+	init_mlx(game);
+	init_moves(game);
+	init_images(game);
+	init_offset(game);
 }
 
 int	main(int argc, char *argv[])
@@ -53,14 +29,9 @@ int	main(int argc, char *argv[])
 		print_error("❌ ERROR: Enter ./so_long [map_file.ber]", NULL);
 	save_map(argv[1], &game);
 	is_valid_map(&game);
-	get_total_collect_count(&game);
-	init_window_size(&game);
-	init_mlx(&game);
-	init_moves(&game);
-	init_images(&game);
-	init_offset(&game);
+	get_map_info(&game);
+	init_setting(&game);
 	draw_map(&game);
-	get_player_idx(&game);
 	mlx_hook(game.win, X_EVENT_KEY_EXIT, 0, &exit_hook, &game);
 	mlx_hook(game.win, X_EVENT_KEY_RELEASE, 0, &main_loop_hook, &game);
 	mlx_loop(game.mlx);
