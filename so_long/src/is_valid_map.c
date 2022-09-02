60ce33ae59db0938ea84023a6da2164b1cbfd9ab
joonhyuk.han <virq@naver.com>
2022-09-03 00:24:17 +0900

    Fix error message to comply with mandatory requirement

--- so_long/src/is_valid_map.c
+++ so_long/src/is_valid_map.c
@@ -6,7 +6,7 @@
 /*   By: joonhan <joonhan@studnet.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 17:20:41 by joonhan           #+#    #+#             */
-/*   Updated: 2022/08/28 21:38:34 by joonhan          ###   ########.fr       */
+/*   Updated: 2022/09/03 00:22:24 by joonhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,12 @@ static void	check_wall(t_game *game)
 				idx / game->map_width == game->map_height - 1)
 			{
 				if (game->map[idx] != '1')
-					print_error("❌ ERROR: Map isn't surrounded by wall.", game);
+					print_error("Error\n❌ Map isn't surrounded by wall.", game);
 			}
 			else if (count == 0 || count == game->map_width - 1)
 			{
 				if (game->map[idx] != '1')
-					print_error("❌ ERROR: Map isn't surrounded by wall.", game);
+					print_error("Error\n❌ Map isn't surrounded by wall.", game);
 			}
 			idx += 1;
 			count += 1;
@@ -46,9 +46,9 @@ static void	check_map_size(t_game *game)
 
 	map_size = ft_strlen(game->map);
 	if (map_size < 15 || (game->map_height < 3 || game->map_width < 3))
-		print_error("❌ ERROR: Map size should be more than 15.", game);
+		print_error("Error\n❌ Map size should be more than 15.", game);
 	if (map_size != game->map_height * game->map_width)
-		print_error("❌ ERROR: Map should be in a rectangle shape.", game);
+		print_error("Error\n❌ Map should be in a rectangle shape.", game);
 	game->map_size = map_size;
 }
 
@@ -59,9 +59,9 @@ static void	check_requirements(t_game *game)
 	player_pos = ft_strchr(game->map, 'P');
 	if (!ft_strchr(game->map, 'C') || !ft_strchr(game->map, 'E') || \
 		player_pos == NULL)
-		print_error("❌ ERROR: Check the number of C, P, E", game);
+		print_error("Error\n❌ Check the number of C, P, E", game);
 	if (ft_strchr(player_pos + 1, 'P'))
-		print_error("❌ ERROR: More than one player.", game);
+		print_error("Error\n❌ More than one player.", game);
 }
 
 void	is_valid_map(t_game *game)
