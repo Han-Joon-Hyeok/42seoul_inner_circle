081ef7fb53485fa3aab9da26f08ca5c48095b7e8
joonhan <joonhan@student.42seoul.kr>
2023-02-06 13:43:46 +0900

    Add module00 ex00

---
+++ cpp-module/00/ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+#include <cctype>
+#include <cstring>
+#include <iostream>
+
+void print_str_to_uppercase(const std::string str) {
+  for (int idx = 0; idx < (int)str.length(); idx += 1) {
+    std::cout << static_cast<char>(toupper(str[idx]));
+  }
+}
+
+int main(int argc, char **argv) {
+  if (argc == 1) {
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+  } else {
+    for (int idx = 1; idx < argc; idx += 1) {
+      print_str_to_uppercase(argv[idx]);
+    }
+  }
+  std::cout << std::endl;
+  return (0);
+}
