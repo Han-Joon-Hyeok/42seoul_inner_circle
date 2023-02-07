6b2b33b795360c026341404f14909228204767ba
joonhan <joonhan@student.42seoul.kr>
2023-02-07 15:05:17 +0900

    refactor: Add utils header and functions

--- cpp-module/00/ex01/main.cpp
+++ cpp-module/00/ex01/main.cpp
@@ -1,48 +1,37 @@
 #include "PhoneBook.hpp"
+#include "utils.hpp"
+#include <ios>
+#include <limits>
 #include <string>
 
-#define RED_TEXT "\033[31m"
-#define GREEN_TEXT "\033[32m"
-#define YELLOW_TEXT "\033[33m"
-#define BLUE_TEXT "\033[34m"
-#define CUT_TEXT "\033[0m"
-#define ERROR_HEADER "[ERROR] "
-#define WARN_HEADER "[WARN] "
-
-enum CommandType { ADD, SEARCH, EXIT, INVALID_COMMAND };
+enum CommandType { ADD, SEARCH, EXIT, NONE };
 
 CommandType getCommandType(std::string command) {
   if (command == "ADD") return (ADD);
   if (command == "SEARCH") return (SEARCH);
   if (command == "EXIT") return (EXIT);
-  return (INVALID_COMMAND);
+  return (NONE);
 }
 
 int main(int argc, char **argv) {
-  (void)argv;
   std::string command;
   PhoneBook my_phone_book;
 
   if (argc != 1) {
-    std::cout << YELLOW_TEXT << ERROR_HEADER << CUT_TEXT
-              << "execute file format should be: [./phonebook]" << std::endl;
+    printErrorMessage(INVALID_EXECUTE_FORMAT, argv[1]);
     return (EXIT_FAILURE);
   }
 
   while (true) {
-    std::cout << GREEN_TEXT << "ENTER COMMAND: [ADD, SEARCH, EXIT] " << CUT_TEXT
-              << std::endl
-              << "> ";
-    std::getline(std::cin, command);
+    showPrompt(COMMAND_PROMPT, command);
 
     if (std::cin.eof()) {
-      std::cout << BLUE_TEXT << "Goodbye!👋" << CUT_TEXT << std::endl;
+      printColorMessage(BLUE_TEXT, "Goodbye!👋");
       break;
     }
 
     if (std::cin.fail()) {
-      std::cout << YELLOW_TEXT << ERROR_HEADER << "Failed to read input"
-                << CUT_TEXT << std::endl;
+      printErrorMessage(FAILED_TO_READ, "");
       break;
     }
 
@@ -51,14 +40,13 @@ int main(int argc, char **argv) {
         /* code */
         break;
       case ADD:
-        /* code */
+        my_phone_book.addNewContact();
         break;
       case EXIT:
-        std::cout << BLUE_TEXT << "Goodbye!👋" << CUT_TEXT << std::endl;
+        printColorMessage(BLUE_TEXT, "Goodbye!👋");
         return (EXIT_SUCCESS);
       default:
-        std::cout << RED_TEXT << WARN_HEADER << CUT_TEXT
-                  << "Invalid command. Please try again." << std::endl;
+        printWarnMessage(INVALID_COMMAND, command);
         break;
     }
   }
