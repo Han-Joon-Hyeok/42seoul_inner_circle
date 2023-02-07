d52a251f3ecd7a4f861119a5cc26eec01b99c7e0
joonhan <joonhan@student.42seoul.kr>
2023-02-07 15:04:39 +0900

    feat: Add  setUserData function

---
+++ cpp-module/00/ex01/Contact.cpp
@@ -0,0 +1,47 @@
+#include "Contact.hpp"
+
+int Contact::getContactIdx(void) { return (idx_); }
+
+void Contact::setContactIdx(int idx) { idx_ = idx; }
+
+void Contact::setFirstName(std::string input) { first_name_ = input; }
+
+void Contact::setUserData(int idx) {
+  std::string input;
+
+  // while (true) {
+  //   if (isInputAllWhitespace(input))
+
+  // }
+  std::cout << "Input your first name: ";
+  std::getline(std::cin, input);
+  std::cout << "HELLO" << std::endl;
+  std::cout << std::endl;
+  setFirstName(input);
+
+  // std::cout << "Input your last name: ";
+  // std::getline(std::cin, input);
+  // std::cout << std::endl;
+  // setLastName(input);
+
+  // std::cout << "Input your nickname: ";
+  // std::getline(std::cin, input);
+  // std::cout << std::endl;
+  // setNickname(input);
+
+  // std::cout << "Input your phone number: ";
+  // std::getline(std::cin, input);
+  // std::cout << std::endl;
+  // setPhoneNumber(input);
+
+  // std::cout << "Input your darkest secret: ";
+  // std::getline(std::cin, input);
+  // std::cout << std::endl;
+  // setDarkestSecret(input);
+
+  setContactIdx(idx);
+}
+
+void Contact::displayUserData(void) {
+  std::cout << "first_name_: " << first_name_ << std::endl;
+}
--- cpp-module/00/ex01/Contact.hpp
+++ cpp-module/00/ex01/Contact.hpp
@@ -1,23 +1,30 @@
 #ifndef CONTACT_HPP
-# define CONTACT_HPP
+#define CONTACT_HPP
 
-# include <iostream>
+#include <iostream>
 
 class Contact {
-  private:
-    int         idx;
-    std::string first_name;
-    std::string last_name;
-    std::string nickname;
-    std::string phone_number;
-    std::string darkest_secret;
+ private:
+  int idx_;
+  std::string first_name_;
+  std::string last_name_;
+  std::string nickname_;
+  std::string phone_number_;
+  std::string darkest_secret_;
 
-  public:
-    void  set_first_name(std::string first_name);
-    void  set_last_name(std::string last_name);
-    void  set_nickname(std::string nickname);
-    void  set_phone_number(std::string phone_number);
-    void  set_darkest_secret(std::string darkest_secret);
+ public:
+  void setFirstName(std::string first_name);
+  void setLastName(std::string last_name);
+  void setNickname(std::string nickname);
+  void setPhoneNumber(std::string phone_number);
+  void setDarkestSecret(std::string darkest_secret);
+
+  void setContactIdx(int idx);
+  int getContactIdx(void);
+
+  void setUserData(int idx);
+
+  void displayUserData(void);
 };
 
 #endif
