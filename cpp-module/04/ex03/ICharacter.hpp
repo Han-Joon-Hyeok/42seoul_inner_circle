f162a999c4597b483968adb99f7c98e37f200a6c
joonhan <joonhan@student.42seoul.kr>
2023-02-26 17:51:47 +0900

    fix: 소멸자에 중괄호 추가
    중괄호를 사용하지 않았더니 vtable 관련 오류 발생

--- cpp-module/04/ex03/ICharacter.hpp
+++ cpp-module/04/ex03/ICharacter.hpp
@@ -1,13 +1,14 @@
 #ifndef ICharacter_HPP
 # define ICharacter_HPP
 
-#include "AMateria.hpp"
 #include <string>
+
+class AMateria;
 
 class ICharacter
 {
 	public:
-		virtual ~ICharacter(void);
+		virtual ~ICharacter(void) {};
 		virtual std::string const& getName(void) const = 0;
 		virtual void equip(AMateria* m) = 0;
 		virtual void unequip(int idx) = 0;
