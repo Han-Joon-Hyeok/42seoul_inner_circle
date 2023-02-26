b638a9eeb36c0e6fd8d9d7b151359f88c3e14f92
joonhan <joonhan@student.42seoul.kr>
2023-02-26 17:51:07 +0900

    feat: learnMateria 함수 조건문 수정
    feat: createMateria 함수 반환값 수정

--- cpp-module/04/ex03/MateriaSource.cpp
+++ cpp-module/04/ex03/MateriaSource.cpp
@@ -33,10 +33,11 @@ MateriaSource& MateriaSource::operator=(MateriaSource const& rhs) {
   return *this;
 }
 void MateriaSource::learnMateria(AMateria* m) {
-  if (m->getType() == "ice" || m->getType() != "cure") {
+  if (m->getType() == "ice" || m->getType() == "cure") {
     for (int idx = 0; idx < SLOT_SIZE; idx += 1) {
       if (slot_[idx] == NULL) {
         slot_[idx] = m->clone();
+        return;
       }
     }
     std::cout << "[MateriaSource] Can't learn more than 4 materias!"
@@ -51,17 +52,12 @@ AMateria* MateriaSource::createMateria(std::string const& type) {
     std::cout << "[MateriaSource] Only ice or cure can create." << std::endl;
     return (0);
   }
-  if (isLearnedMateria(type) == false) {
-    std::cout << "[MateriaSource] Only ice or cure can create." << std::endl;
-    return (0);
-  }
-}
-
-bool MateriaSource::isLearnedMateria(std::string const& type) {
   for (int idx = 0; idx < SLOT_SIZE; idx += 1) {
-    if (slot_[idx]->getType() == type) {
-      return (true);
+    if (slot_[idx] != NULL && slot_[idx]->getType() == type) {
+      return (slot_[idx]->clone());
     }
   }
-  return (false);
+  std::cout << "[MateriaSource] There is no " << type << " in MateriaSource"
+            << std::endl;
+  return (0);
 }
