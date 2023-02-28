b76884305d907806881dffe57faedb2c5529fee0
joonhan <joonhan@student.42seoul.kr>
2023-02-28 14:08:26 +0900

    feat: Add test cases

--- cpp-module/04/ex03/main.cpp
+++ cpp-module/04/ex03/main.cpp
@@ -9,6 +9,10 @@ int main(void) {
   IMateriaSource* src = new MateriaSource();
   src->learnMateria(new Ice());
   src->learnMateria(new Cure());
+  src->learnMateria(new Cure());
+  src->learnMateria(new Cure());
+
+  src->learnMateria(new Cure()); // [MateriaSource] Can't learn more than 4 materias!
 
   ICharacter* me = new Character("me");
 
@@ -17,13 +21,17 @@ int main(void) {
   me->equip(tmp);
   tmp = src->createMateria("cure");
   me->equip(tmp);
-  tmp = src->createMateria("nope");
-  me->equip(tmp);
+  tmp = src->createMateria("nope"); // [MateriaSource] Only ice or cure can create.
+  me->equip(tmp); // [me] Can't equip empty materia!
 
   ICharacter* bob = new Character("bob");
 
   me->use(0, *bob);
   me->use(1, *bob);
+
+  me->use(-1, *bob); // Index -1 is out of range. [0 ~ 3]
+  me->use(5, *bob); // Index 5 is out of range. [0 ~ 3]
+  
   delete bob;
   delete me;
   delete src;
