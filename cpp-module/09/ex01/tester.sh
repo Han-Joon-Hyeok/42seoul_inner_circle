92f0855b337d2ebecab54facdce77596604a838e
joonhan <joonhan@student.42seoul.kr>
2023-03-24 09:19:49 +0900

    test: Add test cases

--- cpp-module/09/ex01/tester.sh
+++ cpp-module/09/ex01/tester.sh
@@ -17,7 +17,24 @@ echo -e '\n./RPN "7 7 * 7 -"'
 echo -e '\n./RPN "1 2 * 2 / 2 * 2 4 - +"'
 ./RPN "1 2 * 2 / 2 * 2 4 - +"
 
+echo -e '\n./RPN "4 2 - 9 /"'
+./RPN "4 2 - 9 /"
+
+echo -e '\n./RPN "4 2 - 9 / 9 + 9 - 9 *"'
+./RPN "4 2 - 9 / 9 + 9 - 9 *"
+
 echo -e "${YELLOW}\n💥 Error Cases 💥${CLEAR}"
 
 echo -e '\n./RPN "(1 + 1)"'
 ./RPN "(1 + 1)"
+
+echo -e '\n./RPN "1 2 3 -"'
+./RPN "1 2 3 -"
+
+echo -e '\n./RPN "1 0 /"'
+./RPN "1 0 /"
+
+echo -e '\n./RPN "42 2 -"'
+./RPN "42 2 -"
+
+echo -e ""
